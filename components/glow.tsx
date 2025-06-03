e71b77d2a4e44a2968a73f6bedc56cef563e80d5
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-06-03 16:04:30 +0530

    text

--- components/glow.tsx
+++ components/glow.tsx
@@ -5,11 +5,22 @@ export default function Glow() {
   return (
     <div className="absolute top-0 right-0 w-full h-full">
       <div className="relative h-screen w-full overflow-hidden md:overflow-x-hidden">
-        <div className="font-geist-mono absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-xl pointer-events-none whitespace-pre-wrap text-center text-9xl font-bold leading-none text-transparent tracking-widest">
-          <span className=" bg-gradient-to-b from-black to-gray-300/80 bg-clip-text">
-            Safira Studio
+        <div className="font-geist-mono absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-fit pointer-events-none whitespace-pre-wrap text-center text-[10rem] font-bold leading-none text-transparent tracking-widest">
+          <span className="mx-auto bg-gradient-to-b from-black to-gray-300/80 bg-clip-text flex flex-col">
+            <span className="bg-gradient-to-t from-black to-gray-300/80 bg-clip-text">
+              <span className="text-[14rem] bg-gradient-to-b from-black to-gray-300/80 bg-clip-text">
+                S
+              </span>
+              afira
+            </span>
+            <span className="bg-gradient-to-b from-black to-gray-300/80 bg-clip-text">
+              <span className="text-[14rem] bg-gradient-to-b from-black to-gray-300/80 bg-clip-text">
+                S
+              </span>
+              tudio
+            </span>
           </span>
-          <span className="font-outline  ">Safira Studio</span>
+          {/* <span className="font-outline  ">Safira Studio</span> */}
         </div>
 
         <div
