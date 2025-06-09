a7a0b9e4b3625230729b205a8df5a3ee792166de
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-06-09 11:13:19 +0530

    overflow-clip

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <section className="w-full">
         <BentoDemo />
       </section>
-      <div>
+      <div className="overflow-clip w-full border border-green-500 relative">
         <CardSwapDemo />
       </div>
     </main>
